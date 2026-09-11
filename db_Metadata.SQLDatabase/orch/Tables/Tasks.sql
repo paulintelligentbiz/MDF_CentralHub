04fdb499718e900c4ae302fefdc6cb74552828a3
paulintelligentbiz <92554231+paulintelligentbiz@users.noreply.github.com>
2026-09-11 09:56:59 -0700

    Committing 2 items from workspace a937b42f-1407-41c9-b5bf-33288dc7f60f

--- db_Metadata.SQLDatabase/orch/Tables/Tasks.sql
+++ db_Metadata.SQLDatabase/orch/Tables/Tasks.sql
@@ -1,6 +1,6 @@
 CREATE TABLE [orch].[Tasks] (
     [TaskName]               VARCHAR (200)  NOT NULL,
-    [Include]                BIT            CONSTRAINT [DF_TranDAG_Include] DEFAULT ((1)) NOT NULL,
+    [Include]                BIT            CONSTRAINT [DF_Tasks_Include] DEFAULT ((1)) NOT NULL,
     [JobName]                VARCHAR (200)  NOT NULL,
     [ObjectName]             VARCHAR (200)  NOT NULL,
     [WorkspaceName]          VARCHAR (200)  NULL,
@@ -16,8 +16,8 @@ CREATE TABLE [orch].[Tasks] (
     CONSTRAINT [PK_Tran_DAG] PRIMARY KEY CLUSTERED ([TaskName] ASC),
     CONSTRAINT [FK_Tasks_Job] FOREIGN KEY ([JobName]) REFERENCES [orch].[Jobs] ([JobName]),
     CONSTRAINT [FK_Tasks_LoggingLevel] FOREIGN KEY ([LoggingLevel]) REFERENCES [log].[LoggingLevel] ([LoggingLevel]),
-    CONSTRAINT [FK_Tasks_ObjectIDs_Job] FOREIGN KEY ([WorkspaceName], [JobName]) REFERENCES [orch].[ObjectIDs] ([WorkspaceName], [ObjectName]),
     CONSTRAINT [FK_Tasks_ObjectIDs_Object] FOREIGN KEY ([WorkspaceName], [ObjectName]) REFERENCES [orch].[ObjectIDs] ([WorkspaceName], [ObjectName]),
+    CONSTRAINT [FK_Tasks_ObjectIDs_ObjectName] FOREIGN KEY ([WorkspaceName], [ObjectName]) REFERENCES [orch].[ObjectIDs] ([WorkspaceName], [ObjectName]),
     CONSTRAINT [FK_Tasks_TaskType] FOREIGN KEY ([TaskType]) REFERENCES [orch].[TaskType] ([TaskType])
 );
 
